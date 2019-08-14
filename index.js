21c8dee3396c8b1c0009e9a8ba67640aa181ff94
Sean Yim <superjjang89@gmail.com>
2019-08-14 10:21:47 -0400

    adds express

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const http = require('http');
+// const http = require('http');
+//replace http with express
+const express = require('express');
 const Todo = require('./models/Todo');
-const server = http.createServer((req, res)=>{
+//create the server and call it 'app'
+const app = express();
+//create a variable for the port number
+const port = 3000;
+
+// const server = http.createServer((req, res)=>{
+    //replace with app.get()
+app.get('/todos', (req, res)=>{
     console.log("You've got a request");
     const allTodos = Todo.getAll();
     allTodos
         .then((data)=> {
             console.log('omg its data');
             console.log(data);
-            res.end(JSON.stringify(data));
+            // res.end(JSON.stringify(data));
+            res.json(data);
         })
     // console.log("\n\n================");
     // console.log(allTodos);
     // res.end(allTodos);
 });
 
-server.listen(7000);
+app.listen(port);
 
 
 
